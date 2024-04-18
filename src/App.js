a5fa57c7148d398c5f178b0be2ebcc3f81035d70
Abhishekreju2525 <abhishekreju007@gmail.com>
2024-04-18 12:26:56 +0530

    Register function added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import ProtectedRoute from "./pages/ProtectedRoute";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<LoginPage></LoginPage>}></Route>
+        <Route path="/signup" element={<RegisterPage></RegisterPage>}></Route>
         <Route path="/home" element={<ProtectedRoute><HomePage/></ProtectedRoute>}></Route>
       </Routes>
     </>
--- src/context/UserStore.js
+++ src/context/UserStore.js
@@ -5,6 +5,12 @@ export default function UserStore({ children }) {
   const [user, setUser] = useState(null);
   const [apiUserData, setapiUserData] = useState([]);
   const userApiUrl = "https://66209a873bf790e070b0175d.mockapi.io/api/v1/user";
+  const userToAdd = {
+    name : "Test",
+    email:'test@gmail.com',
+    avatar:"https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1070.jpg"
+}
+
   function loginUser(email, password) {
     const foundUser = apiUserData.find(
       (item) => item.email === email && item.password === password
@@ -19,6 +25,17 @@ export default function UserStore({ children }) {
       return false; // Or set some state indicating failed login
     }
   }
+  async function addUser(formData) {
+    let res = await fetch(userApiUrl, {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(formData),
+    });
+
+    let newData = await res.json();
+    console.log("Data from api", newData);
+  }
+
   async function userApiCall() {
     const res = await fetch(userApiUrl);
     const data = await res.json();
@@ -30,7 +47,7 @@ export default function UserStore({ children }) {
   }, []);
 
   return (
-    <UserContext.Provider value={{ user, setUser, loginUser }}>
+    <UserContext.Provider value={{ user, setUser, loginUser,addUser }}>
       {children}
     </UserContext.Provider>
   );
